49959bb7ae8f64d5c2b20a11be9955230ffc66f1
seven <sevenjustfine@qq.com>
2019-10-14 18:33:40 +0800

    ...

---
+++ pages/assignmentTask/index.vue
@@ -0,0 +1,87 @@
+<template>
+    <view class="container">
+        <view class="title">分配传款人</view>
+        <view class="input-style">
+            <input type="text" placeholder="选择买手或设计师">
+            <i>图标</i>
+        </view>
+        <view class="title">款图评分时间</view>
+        <view class="input-style">
+            <input type="text" placeholder="选择买手或设计师">
+            <i>图标</i>
+        </view>
+        <view class="title">款图评分规则</view>
+        <view class="input-style">
+            <input type="text" >
+            <i>图标</i>
+        </view>
+
+
+        <view class="fixed">
+            <view>重置</view>
+            <view>确定</view>
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        name: "INDEX"
+    }
+</script>
+
+<style scoped lang="scss">
+
+    .container{
+        width: $width;
+        margin: 0 auto;
+        .title{
+            font-size: 28upx;
+            color: #333;
+            margin: 20upx 0;
+        }
+        .fixed{
+            padding: 20upx 120upx;
+            border-top:2upx solid #e2e2e2;
+           display: flex;
+            justify-content: space-between;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            font-size: 28upx;
+            view{
+                width: 204upx;
+                height: 76upx;
+                border-radius: 48upx;
+                border: 2upx solid #e2e2e2;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                &:last-child{
+                    border-color: #f44444;
+                    background-color: #f44444;
+                    color: #fff;
+                }
+            }
+        }
+        .input-style{
+            display: flex;
+            justify-content: space-between;
+            height: 76upx;
+            border-radius: 40upx;
+            background-color: #F0F0F0;
+            padding: 0 20upx ;
+            input{
+                height: 100%;
+                text-indent: 30upx;
+                font-size: 28upx;
+            }
+
+            i{
+                font-size: 20upx;
+                align-self: center;
+            }
+        }
+    }
+</style>
--- pages/taskSquare/taskSquare.vue
+++ pages/taskSquare/taskSquare.vue
@@ -1,9 +1,9 @@
 <template>
 	<view>
-		<navBar  
-		title="任务广场" 
+		<navBar
+		title="任务广场"
 		fontColor="#222">
-		    <view class="icon_distribution" slot="right">分配</view>
+			<view class="icon_distribution" slot="right"> <navigator hover-class="navigator-hover" url="/pages/assignmentTask/index" open-type="navigate">分配</navigator></view>
 		</navBar>
 		<sun-tab :value.sync="swiperIndex" :tabList="tabSwiperList" activeColor="#f44444">
 			<view slot="right" class="sx" @click="showDrawer()">筛选</view>
@@ -34,17 +34,69 @@
 				</view>
 			</swiper-item>
 		 </swiper>
-		 
+
 		 <view class="fixed">
 		 		<view><radio /> 全选</view>
 				<view>合计： <text>2</text> 任务 <text>20</text>款</view>
 				<view>接受任务</view>
 		 </view>
-		 
-		 
+
+
 		 <uni-drawer :visible="showRigth" mode="right" @close="closeDrawer()">
-			sss
-		 	<view class="close"><button type="default" @click="closeDrawer">关闭Drawer</button></view>
+			 <view class="drawer-content">
+				 <view style="text-align: center;font-size: 36upx;font-weight: bold;">筛选</view>
+				 <view class="class-item">
+					 <view>年份</view>
+					 <view class="list">
+						 <text class="active">2019</text>
+						 <text>2020</text>
+					 </view>
+				 </view>
+				 <view class="class-item">
+					 <view>季节</view>
+					 <view  class="list">
+						 <text class="active">春季</text>
+						 <text>夏季</text>
+						 <text>秋季</text>
+						 <text>冬季</text>
+					 </view>
+				 </view>
+				 <view class="class-item">
+					 <view>波段</view>
+					 <view class="list">
+						 <text class="active">第一波段</text>
+						 <text>第二波段</text>
+						 <text>第三波段</text>
+						 <text>第四波段</text>
+					 </view>
+				 </view>
+				 <view class="class-item">
+					 <view>分配时间 （接收任务时间）</view>
+					 <view class="picker">
+						 <view>
+							 <picker mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
+								 <view class="uni-input">{{date}}</view>
+							 </picker>
+						 </view>
+						 <text>前</text>
+					 </view>
+				 </view>
+				 <view class="class-item">
+					 <view>买手</view>
+					 <view class="picker">
+						 <view>
+							 <picker mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
+								 <view class="uni-input">{{date}}</view>
+							 </picker>
+						 </view>
+					 </view>
+				 </view>
+			 </view>
+
+			 <view class="fixed-list">
+				 <view>重置</view>
+				 <view class="end">确定</view>
+			 </view>
 		 </uni-drawer>
 	</view>
 </template>
@@ -71,7 +123,7 @@
 				this.swiperIndex = e.target.current;
 			},
 			onPreview(){
-				
+
 			},
 			showDrawer(){
 				this.showRigth = true
@@ -101,7 +153,7 @@
 			align-items: center;
 			color: #fff;
 		}
-		
+
 		& > view:nth-child(2){
 			font-size: 26upx;
 			align-self: center;
@@ -110,7 +162,7 @@
 				padding: 0 15upx;
 			}
 		}
-		
+
 		& > view:first-child{
 			font-size: 26upx;
 			display: flex;
@@ -128,7 +180,7 @@
 			display: flex;
 			padding: 20upx 0;
 			justify-content: space-between;
-	
+
 			& > view:first-child{
 				display: flex;
 				font-size: 26upx;
@@ -172,7 +224,7 @@
 		align-self: center;
 		margin-right: 20upx;
 	}
-	
+
 	.nav-list{
 		display: flex;
 		justify-content: space-between;
@@ -188,5 +240,82 @@
 		justify-content: center;
 		align-items: center;
 		border-radius: 5upx;
+	}
+	.fixed-list{
+		position: fixed;
+		bottom: 0;
+		display: flex;
+		justify-content: space-between;
+		width: calc(100% - 240upx);
+		padding: 20upx 120upx;
+		height: 50upx;
+		border-bottom: 2upx solid #e2e2e2;
+		view{
+			width: 120upx;
+			height: 100%;
+			font-size: 26upx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border: 2upx solid #e2e2e2;
+			border-radius: 5upx;
+		}
+		.end{
+			background: #f44444;
+			border-color: #f44444 ;
+			color: #fff;
+		}
+	}
+	.drawer-content{
+		padding: 30px 20upx 0;
+		.class-item{
+			margin-bottom: 40upx;
+			& > view:first-child{
+				font-size: 28upx;
+				margin-bottom: 15upx;
+			}
+			.picker{
+				display: flex;
+				justify-content: space-between;
+				& > view{
+					height: 60upx;
+					background: #efefef;
+					border-radius: 60rpx;
+					width: 90%;
+
+					picker{
+						height: 100%;
+						color: #222;
+						font-size: 30upx;
+						text-indent: 15upx;
+						.uni-input{
+							line-height: 60upx;
+						}
+					}
+				}
+				text{
+					font-size: 28upx;
+					align-self: center;
+				}
+			}
+			.list{
+				display: flex;
+				flex-wrap: wrap;
+				text{
+					font-size: 26upx;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					padding: 5upx 15upx;
+					margin-right: 10upx;
+					margin-bottom: 10upx;
+					border-radius: 5upx;
+					&.active{
+						background-color: #f44444;
+						color: #fff;
+					}
+				}
+			}
+		}
 	}
 </style>
